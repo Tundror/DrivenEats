1152e2d1a9d22c888a57549ff08ba3e705e753fb
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-01-05 15:49:28 -0300

    selecao individual com check completa

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
-function selecionar(opcaoSelecionada){
-const botaoAnterior = document.querySelector('.selecionado');
-console.log(botaoAnterior);
-if(botaoAnterior !== null){
-botaoAnterior.classList.remove('selecionado');
+function selecionar1(opcaoSelecionada, idCheck){
+    const botaoAnterior = document.querySelector('.selecionado-f1');
+    if(botaoAnterior !== null){
+        botaoAnterior.classList.remove('selecionado-f1');
+    }
+    const botaoSelecionado = document.querySelector(opcaoSelecionada);
+    botaoSelecionado.classList.add('selecionado-f1');
+    const checkAnterior = document.querySelector('.checkSelecionado-f1')
+    if(checkAnterior !== null){
+        checkAnterior.classList.remove('checkSelecionado-f1')
+    }
+    const checkSelecionado = document.querySelector(idCheck);
+    checkSelecionado.classList.add('checkSelecionado-f1');
 }
-console.log(opcaoSelecionada);
-const botaoSelecionado = document.querySelector(opcaoSelecionada);
-console.log(botaoSelecionado);
-botaoSelecionado.classList.add('selecionado');
-
+function selecionar2(opcaoSelecionada, idCheck){
+    const botaoAnterior = document.querySelector('.selecionado-f2');
+    if(botaoAnterior !== null){
+        botaoAnterior.classList.remove('selecionado-f2');
+    }
+    const botaoSelecionado = document.querySelector(opcaoSelecionada);
+    botaoSelecionado.classList.add('selecionado-f2');
+    const checkAnterior = document.querySelector('.checkSelecionado-f2')
+    if(checkAnterior !== null){
+        checkAnterior.classList.remove('checkSelecionado-f2')
+    }
+    const checkSelecionado = document.querySelector(idCheck);
+    checkSelecionado.classList.add('checkSelecionado-f2');
+}
+function selecionar3(opcaoSelecionada, idCheck){
+    const botaoAnterior = document.querySelector('.selecionado-f3');
+    if(botaoAnterior !== null){
+        botaoAnterior.classList.remove('selecionado-f3');
+    }
+    const botaoSelecionado = document.querySelector(opcaoSelecionada);
+    botaoSelecionado.classList.add('selecionado-f3');
+    const checkAnterior = document.querySelector('.checkSelecionado-f3')
+    if(checkAnterior !== null){
+        checkAnterior.classList.remove('checkSelecionado-f3')
+    }
+    const checkSelecionado = document.querySelector(idCheck);
+    checkSelecionado.classList.add('checkSelecionado-f3');
 }
